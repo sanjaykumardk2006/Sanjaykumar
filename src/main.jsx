2883fa8aee468a9e5fe30cb922f13da28cd14cd9
sanjaykumardk2006 <sanjaykumardk2006@gmail.com>
2026-06-22 11:26:25 +0530

    add the vercel alalysis

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { LazyMotion, domAnimation } from 'framer-motion'
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
