63da03875be4924c81f5d18d3ce1aaaafad050b6
sanjaykumardk2006 <sanjaykumardk2006@gmail.com>
2026-06-22 11:36:23 +0530

    added speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <LazyMotion features={domAnimation}>
       <App />
+      <SpeedInsights />
     </LazyMotion>
   </React.StrictMode>
 )
